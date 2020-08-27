4747390ecac89c83dbf3d6229884b8d9bb0502d9
nnsncl <nicolas.mgta@gmail.com>
2020-08-27 23:29:18 +0200

    Semicolons :(

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export default function SignIn(){
     return (
         <h1>Sign In</h1>
-    )
-}
+    );
+};
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export default function SignUp(){
     return (
         <h1>Sign Up</h1>
-    )
-}
+    );
+};
