0ba85c94a782bb0445b2473cf0f7149f34dabd93
nnsncl <nicolas.mgta@gmail.com>
2020-08-27 23:17:07 +0200

    Nest Routes into AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, SignIn, SignUp } from './pages';
+import { AuthProvider } from './auth/Auth';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/signin' component={SignIn} />
-        <Route exact path='/signup' component={SignUp} />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/signin' component={SignIn} />
+          <Route exact path='/signup' component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
+
   )
 }
 
