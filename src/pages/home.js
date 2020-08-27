fac0c933c66a2a9fc38b0c252f7c9ef057513bde
nnsncl <nicolas.mgta@gmail.com>
2020-08-27 21:48:31 +0200

    Pages Scaffolding

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import firebaseApp from '../firebase'
+
+export default function Home() {
+    return (
+        <>
+            <h1>Home</h1>
+            <button onClick={() => firebaseApp.auth().signOut()} >Sign out</button>
+        </>
+    )
+}
