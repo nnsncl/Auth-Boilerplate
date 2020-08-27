3da0a0560dc761fa712526cad515f9c4575efd71
nnsncl <nicolas.mgta@gmail.com>
2020-08-27 23:57:14 +0200

    Signin Auth

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
+import { withRouter, Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+import firebaseApp from '../firebase';
+import { AuthContext } from '../auth/Auth';
 
-export default function SignIn(){
-    return (
-        <h1>Sign In</h1>
+const SignIn = ({ history }) => {
+    const handleSignIn = useCallback(
+        async event => {
+            event.preventDefault();
+            const { email, password } = event.target.elements;
+
+            try {
+                await firebaseApp
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value);
+                history.push('/');
+            } catch (error) {
+                alert(error);
+            }
+        }, [history]
+
     );
+
+    const { currentUser } = useContext(AuthContext);
+
+    if (currentUser) {
+        return <Redirect to='/' />;
+    }
+
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSignIn} >
+                <label htmlFor='email' >Email</label>
+                <input name='email' type='email' placeholder='Email address' />
+                <label htmlFor='password' >Email</label>
+                <input name='password' type='password' placeholder='Password' />
+                <button type='submit' >Sign In</button>
+            </form>
+            <Link to='/signup' >Sign Up</Link>
+        </div>
+    );
+
 };
+
+export default withRouter(SignIn);
