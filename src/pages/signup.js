aabce331e7a80dfa70a9237949f008976681d871
nnsncl <nicolas.mgta@gmail.com>
2020-08-27 23:40:14 +0200

    Signup auth

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { withRouter } from 'react-router';
+import firebaseApp from '../firebase';
 
-export default function SignUp(){
+const SignUp = ({ history }) => {
+    const handleSignUp = useCallback(async event => {
+        event.preventDefault();
+        const { email, password } = event.target.elements;
+
+        try {
+            await firebaseApp
+                .auth()
+                .createUserWithEmailAndPassword(email.value, password.value);
+            history.push('/');
+        } catch (error) {
+            alert(error);
+        }
+    }, [history])
+
     return (
-        <h1>Sign Up</h1>
+        <div>
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSignUp}>
+                <label htmlFor='email' >Email</label>
+                <input name='email' type='email' placeholder='Email address' />
+                <label htmlFor='password' >Email</label>
+                <input name='password' type='password' placeholder='Password' />
+                <button type='submit' >Sign Up</button>
+            </form>
+        </div>
     );
 };
+
+export default withRouter(SignUp);
